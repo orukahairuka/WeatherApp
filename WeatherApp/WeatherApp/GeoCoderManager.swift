24a8b797c1a3cad2753222c169aa23e60e1e4629
irukahaoruka <erika03301109@gmail.com>
2024-09-25 17:07:18 +0900

    緯度軽度のapiリクエストを投げる

---
+++ WeatherApp/WeatherApp/GeoCoderManager.swift
@@ -0,0 +1,42 @@
+//
+//  GeoCoderManager.swift
+//  WeatherApp
+//
+//  Created by 櫻井絵理香 on 2024/09/25.
+//
+
+import Foundation
+import Alamofire
+
+func fetchCoordinates() {
+    let url = "https://map.yahooapis.jp/geocode/cont/V1/contentsGeoCoder"
+    let query = "六本木"
+    
+    //エンコードの処理
+    guard let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+        print("エンコードに失敗しました")
+        return
+    }
+    
+    let parameters:[String: String] = [
+        "appid":"dj00aiZpPVo5RTFsZ1pXUWNvNCZzPWNvbnN1bWVyc2VjcmV0Jng9MjE-",
+        "query": encodedQuery,
+        "category": "address",
+        "output": "json"
+    ]
+    
+    AF.request(url, parameters: parameters).responseDecodable(of: Welcome.self) { response in
+        switch response.result {
+        case .success(let data):
+            if let coordinates = data.feature.first?.geometry.coordinates {
+                print("Coordinates:\(coordinates)")
+            } else {
+                print("座標取得できず")
+            }
+        case .failure(let error):
+            print("api取得失敗")
+        }
+    }
+    
+}
+
--- WeatherApp/WeatherApp/Models/GeoCoderManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  GeoCoderManager.swift
-//  WeatherApp
-//
-//  Created by 櫻井絵理香 on 2024/09/25.
-//
-
-import Foundation
