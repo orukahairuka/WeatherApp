c796b64632c4002b146f0e2d4e09654a305e76aa
irukahaoruka <erika03301109@gmail.com>
2024-09-26 19:32:45 +0900

    修正

--- WeatherApp/WeatherApp/Models/GeoCoderManager.swift
+++ WeatherApp/WeatherApp/Models/GeoCoderManager.swift
@@ -8,33 +8,41 @@
 import Foundation
 import Alamofire
 
-func fetchCoordinates(query:String) {
-    let url = "https://map.yahooapis.jp/geocode/cont/V1/contentsGeoCoder"
-
-    //エンコードの処理
-    guard let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
-        print("エンコードに失敗しました")
-        return
+class GeoCoderManager {
+    private var weatherManager: WeatherManager
+    
+    init(weatherManager: WeatherManager) {
+        self.weatherManager = weatherManager
     }
     
-    let parameters:[String: String] = [
-        "appid":"dj00aiZpPVo5RTFsZ1pXUWNvNCZzPWNvbnN1bWVyc2VjcmV0Jng9MjE-",
-        "query": encodedQuery,
-        "category": "address",
-        "output": "json"
-    ]
-    
-    AF.request(url, parameters: parameters).responseDecodable(of: GeoWelcome.self) { response in
-        switch response.result {
-        case .success(let data):
-            if let coordinates = data.feature.first?.geometry.coordinates {
-                print("Coordinates:\(coordinates)")
-                fetchWether(coordinate: coordinates)
-            } else {
-                print("座標取得できず")
+    func fetchCoordinates(query:String) {
+        let url = "https://map.yahooapis.jp/geocode/cont/V1/contentsGeoCoder"
+
+        //エンコードの処理
+        guard let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            print("エンコードに失敗しました")
+            return
+        }
+        
+        let parameters:[String: String] = [
+            "appid":"dj00aiZpPVo5RTFsZ1pXUWNvNCZzPWNvbnN1bWVyc2VjcmV0Jng9MjE-",
+            "query": encodedQuery,
+            "category": "address",
+            "output": "json"
+        ]
+        
+        AF.request(url, parameters: parameters).responseDecodable(of: GeoWelcome.self) { response in
+            switch response.result {
+            case .success(let data):
+                if let coordinates = data.feature.first?.geometry.coordinates {
+                    print("Coordinates:\(coordinates)")
+                    self.weatherManager.fetchWether(coordinate: coordinates)
+                } else {
+                    print("座標取得できず")
+                }
+            case .failure(let error):
+                print("api取得失敗")
             }
-        case .failure(let error):
-            print("api取得失敗")
         }
     }
     
